71fa430ba5bc6b2c9d697ceca283607d99e737a5
Sachitha Hirushan <shpsachitha@gmail.com>
2021-12-13 13:16:04 +0530

    wip

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var color = {
+    red: 250,
+    blue: 100,
+    green: 10
+};
+console.log(color);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface Color{
+    red: number;
+    blue: number;
+    green: number;
+}
+
+
+const color: Color = {
+    red: 250,
+    blue: 100,
+    green: 10
+}
+
+console.log(color);
