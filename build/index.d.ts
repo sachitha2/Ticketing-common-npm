7c941ada58fc4119034e7545c2b52b577ad174bb
Sachitha Hirushan <shpsachitha@gmail.com>
2021-12-13 19:25:00 +0530

    update

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,7 +1,10 @@
-interface Color {
-    red: number;
-    blue: number;
-    green: number;
-}
-declare const color: Color;
-export default color;
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-auth-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
